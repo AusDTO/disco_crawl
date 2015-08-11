98a1a58521ccf74350c1e18cd3036232aaf8f2ed
nokout <nigel.o'keefe@dto.gov.au>
2015-08-11 23:25:00 +1000

    Completed building csv with one minor issue reaaining to properly set the servingcontent flag. just need to call the data.gov.au update api now.

--- loadHostData.js
+++ loadHostData.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var http = require('http');
 var Promise = require('bluebird');
 var request = Promise.promisify(require('request'));
@@ -5,71 +6,29 @@ var logger = require('./config/logger');
 var conf = require('./config/config');
 var crawlDb = require('./lib/ormCrawlDb');
 var moment = require('moment');
+var csvParse = require('csv');
+var json2csv = require('json2csv');
+
 var join = Promise.join;
 var apiKey = require('./config/apiKey');
 
 logger.info('CrawlJob Settings: ' + JSON.stringify(conf._instance) + '\n');
 
 
-var dgaOutputUrl = 'https://data.gov.au/dataset/08557cab-df37-45b1-9f19-5f85efcf05bd/resource/1e44826f-ae14-40ad-b7e2-125dc82a36bd/download/hosts.csv';
-var dgaBlacklistUrl = 'https://data.gov.au/dataset/08557cab-df37-45b1-9f19-5f85efcf05bd/resource/6239de78-b28c-45a8-add2-413ed6a6f88a/download/blacklist.csv';
-var dgaWhitelistUrl = 'https://data.gov.au/dataset/08557cab-df37-45b1-9f19-5f85efcf05bd/resource/8aea9d2f-fd21-42ef-8cc5-bf9db3533bf8/download/whitelist.csv';
-
-function buildHostdata(existingHosts, blackList, whitelist) {
-  //
-
-  crawlDb.connect()
-  .then(
-    function() {
-      logger.debug('do');
-      crawlDb.getHosts()
-        .then(function(results) {
-          var resultString = 'HOST,DUPLICATE\n';
-          results.forEach(function(result) {
-            resultString += result.host + ',' + (result.duplicate === true).toString() + '\n';
-          });
-          request('http://demo.ckan.org/api/3/action/resource_update', function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-              logger.debug(body); // Show the HTML for the Google homepage.
-            }
-          });
-        });
-    })
-  .catch(function(e) {
-    logger.error(e);
-  });
-}
-
-
-
-//Add an extra database function to fetch the host list and mark duplicates
 crawlDb.getHosts = function() {
   orm = this;
   return new Promise(function(resolve, reject) {
-    orm.db.query('SELECT host, min("httpCode") FROM "webDocuments" GROUP BY host;').spread(function(results, metadata) {
-      results = results.sort();
+    orm.db.query('SELECT host, min("httpCode") as "minHttpCode" FROM "webDocuments" GROUP BY host;').spread(function(results, metadata) {
+      //results = results.sort();
+      //logger.debug('DB response: ' + JSON.stringify(results));
+      logger.debug('DB metadata: ' + JSON.stringify(metadata));
       // Results will be an array and metadata will contain the number of affected rows
-      results.forEach(function(item, index, array) {
-        //          logger.debug(item.host);
-        if (item.host.search('^www\.') >= 0) {
-          var duplicateSearch = item.host.slice(4);
-          var duplicateSet = array.filter(function(row) {
-            return row.host === duplicateSearch;
-          });
-
-          if (duplicateSet.length > 0) {
-            item.duplicate = true;
-          } else {
-            item.duplicate = false;
-          }
-        } else {
-          item.duplicate = false;
-        }
-      });
       resolve(results);
     });
   });
 };
+
+
 
 function shouldJobRun(lastOutputUpdate, lastBlacklistUpdate, lastWhitelistUpdate) {
   if (lastOutputUpdate.isBefore(moment().subtract(7, 'days'))) {
@@ -95,12 +54,19 @@ function getResource(options) {
       })
       .spread(function(response, body) {
         logger.debug('Getting ' + options.name);
-
-        resolve({
-          name: options.name,
-          response: response,
-          body: body
+        logger.debug('Result: ' + body);
+        csvParse.parse(body, {
+          columns: true,
+          skip_empty_lines: true
+        }, function(err, objectBody) {
+          console.log(objectBody);
+          resolve({
+            name: options.name,
+            response: response,
+            body: objectBody
+          });
         });
+
       })
       .catch(function(err) {
         logger.error(err);
@@ -108,7 +74,82 @@ function getResource(options) {
   });
 }
 
+function buildHostdata(existingHosts, newHosts, blacklist, whitelist) {
+  return new Promise(function(resolve, reject) {
+    newHosts.forEach(function(host) {
+      host.whitelisted = false;
+      host.blacklisted = false;
+      host.wwwduplicate = false;
+      if (host.minHttpCode < 300) {
+        host.servescontent = true;
+      } else {
+        host.servescontent = false;
+      }
+      blacklist.forEach(function(blacklistHost) {
+        if (host.host === blacklistHost.host) {
+          host.blacklisted = true;
+          logger.debug('blacklisting: ' + JSON.stringify(host));
+        }
+      });
+    });
+    whitelist.forEach(function(whitelistHost) {
+      whitelistHost = {
+        host: whitelistHost.host,
+        whitelisted: true,
+        blacklisted: false,
+        minHttpCode: null,
+        wwwduplicate: false,
+        servescontent: null
+      };
+      logger.debug('whitelisting: ' + JSON.stringify(whitelistHost));
+      newHosts.push(whitelistHost);
+    });
+    var deduplicateddHosts = [];
+    newHosts.forEach(function(item, index, array) {
+      if (item.host) {
+        //logger.debug('DB row: ' + JSON.stringify(item));
+        if (item.host.search('^www\.') >= 0) {
+          var duplicateSearch = item.host.slice(4);
+          var duplicateSet = array.filter(function(row) {
+            return row.host === duplicateSearch;
+          });
 
+          if (duplicateSet.length > 0) {
+            item.wwwduplicate = true;
+            //logger.debug('Duplicate host: ' + item.host);
+          }
+        }
+        //logger.debug(JSON.stringify(item) + '-----' + deduplicateddHosts.length);
+        deduplicateddHosts.push(item);
+    //TODO: servingcontent is not being set correctly
+    //TODO: Should not need to do this redundant copy
+    //TODO: Compare old and new before uploading. That might be a diff function
+      }
+    });
+
+    //logger.debug('Final Result... \n' + JSON.stringify(newHosts, null, 2));
+    resolve(deduplicateddHosts);
+  });
+}
+
+function createCsv(hostsDataObject) {
+  return new Promise(function(resolve, reject) {
+    json2csv({
+      data: hostsDataObject
+    }, function(err, csv) {
+      if (err) {
+        logger.debug('err: ' + err);
+        reject(err);
+      }
+      //logger.debug('csv' + csv);
+      resolve(csv);
+    });
+  });
+}
+
+
+
+//do a promise join to request/query each resource in parallel and process when all promises resolve.
 join(getResource({
     name: 'output',
     url: 'https://data.gov.au/dataset/08557cab-df37-45b1-9f19-5f85efcf05bd/resource/1e44826f-ae14-40ad-b7e2-125dc82a36bd/download/hosts.csv',
@@ -122,22 +163,34 @@ join(getResource({
     url: 'https://data.gov.au/dataset/08557cab-df37-45b1-9f19-5f85efcf05bd/resource/8aea9d2f-fd21-42ef-8cc5-bf9db3533bf8/download/whitelist.csv',
     apiKey: apiKey
   }),
-  function(outputResponse, blacklistResponse, whitelistResponse) {
-    logger.debug('output: ' + outputResponse.body);
+  crawlDb.getHosts(),
+  function(outputResponse, blacklistResponse, whitelistResponse, newHosts) {
+    //logger.debug('output: ' + outputResponse.body);
     logger.debug('Joining');
-    logger.debug('blacklist: ' + blacklistResponse.body);
-    logger.debug('whitelist: ' + whitelistResponse.body);
+    //logger.debug('blacklist: ' + blacklistResponse.body);
+    //logger.debug('whitelist: ' + whitelistResponse.body);
 
     var lastOutputUpdate = moment(new Date(outputResponse.response.headers['last-modified'])); //Format: Tue, 04 Aug 2015 15:20:58 GMT
     var lastBlacklistUpdate = moment(new Date(blacklistResponse.response.headers['last-modified'])); //, 'ddd, DD MMM YYYY HH:MM:SS ZZZ'); //Format: Tue, 04 Aug 2015 15:20:58 GMT
     var lastWhitelistUpdate = moment(new Date(whitelistResponse.response.headers['last-modified'])); // 'ddd, DD MMM YYYY HH:MM:SS ZZZ'); //Format: Tue, 04 Aug 2015 15:20:58 GMT
     logger.debug('Dates... \noutput:  \t' + lastOutputUpdate.format() + ' \nblacklist:\t' + lastBlacklistUpdate.format() + '\nwhitelist:\t' + lastWhitelistUpdate.format());
 
-
-    if (shouldJobRun(lastOutputUpdate, lastBlacklistUpdate, lastWhitelistUpdate)) {
+    if (shouldJobRun(lastOutputUpdate, lastBlacklistUpdate, lastWhitelistUpdate) || conf.get('debug') === true) {
+      buildHostdata(outputResponse.body, newHosts, blacklistResponse.body, whitelistResponse.body)
+        .then(function(hostDataObject) {
+          createCsv(hostDataObject)
+            .then(function(csvString) {
+              fs.writeFile('hosts.csv', csvString);
+              //sendCsv(csvString);
+            })
+            .catch(function(e) {
+              logger.error(e);
+            });
+        })
+        .catch(function(e) {
+          logger.error(e);
+        });
       logger.debug('Updating Host Data');
-
-
     } else {
       logger.debug('No Update Required');
     }
